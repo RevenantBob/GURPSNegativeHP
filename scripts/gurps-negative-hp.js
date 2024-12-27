8259f6e6a455244173c49c0378de80454c61ac17
Merlin Avery <merlin.avery@beyondidentity.com>
2024-12-27 12:28:21 -0500

    Final release changes

--- scripts/gurps-negative-hp.js
+++ scripts/gurps-negative-hp.js
@@ -1,6 +1,10 @@
 
   /* -------------------------------------------- */
 
+
+  Hooks.once("init", () => {
+    console.log("GURPS Negative HP | Initializing GURPS Negative HP module");
+
   /**
    * Handle how changes to a Token attribute bar are applied to the Actor.
    * This allows for game systems to override this behavior and deploy special logic.
@@ -10,25 +14,6 @@
    * @param {boolean} isBar       Whether the new value is part of an attribute bar, or just a direct value
    * @returns {Promise<documents.Actor>}  The updated Actor document
    */
-  /**async modifyTokenAttribute(attribute, value, isDelta=false, isBar=true) {
-    const attr = foundry.utils.getProperty(this.system, attribute);
-    const current = isBar ? attr.value : attr;
-    const update = isDelta ? current + value : value;
-    if ( update === current ) return this;
-
-    // Determine the updates to make to the actor data
-    let updates;
-    if ( isBar ) updates = {[`system.${attribute}.value`]: Math.min(update, attr.max)};
-    else updates = {[`system.${attribute}`]: update};
-
-    // Allow a hook to override these changes
-    const allowed = Hooks.call("modifyTokenAttribute", {attribute, value, isDelta, isBar}, updates);
-    return allowed !== false ? this.update(updates) : this;
-  }**/
-  Hooks.once("init", () => {
-    console.log("GURPS Negative HP | Initializing GURPS Negative HP module");
-
-    // Assuming 'SomeClass' is globally available
     Actor.prototype.modifyTokenAttribute = async function(attribute, value, isDelta=false, isBar=true) {
       const attr = foundry.utils.getProperty(this.system, attribute);
       const current = isBar ? attr.value : attr;
@@ -44,26 +29,4 @@
       const allowed = Hooks.call("modifyTokenAttribute", {attribute, value, isDelta, isBar}, updates);
       return allowed !== false ? this.update(updates) : this;
     }
-
-    // Open module API
-    window.modifyTokenAttribute = GURPSNegativeHP.modifyTokenAttribute;
   });
-
-  class GURPSNegativeHP {
-  
-    static async modifyTokenAttribute(attribute, value, isDelta=false, isBar=true) {
-      const attr = foundry.utils.getProperty(this.system, attribute);
-      const current = isBar ? attr.value : attr;
-      const update = isDelta ? current + value : value;
-      if ( update === current ) return this;
-  
-      // Determine the updates to make to the actor data
-      let updates;
-      if ( isBar ) updates = {[`system.${attribute}.value`]: Math.min(update, attr.max)};
-      else updates = {[`system.${attribute}`]: update};
-  
-      // Allow a hook to override these changes
-      const allowed = Hooks.call("modifyTokenAttribute", {attribute, value, isDelta, isBar}, updates);
-      return allowed !== false ? this.update(updates) : this;
-    }
-  }
